aefa89a95b56e679d3236df70ca4aadd4f484c61
meb-engi <mebish555@gmail.com>
2026-05-05 16:54:05 +0800

    improved index.html landing page improved darkmode.js changed loading screen

--- javaScript/darkmode.js
+++ javaScript/darkmode.js
@@ -4,15 +4,16 @@ const themeSwitch = document.getElementById("theme-switch")
 const enableDarkMode = () => {
     document.body.classList.add("darkmode")
     localStorage.setItem("darkmode", "active")
-    document.getElementById("apta").src = "images/logo/APTA_LOGO.webp"; //changes logo
-    document.getElementById("bglogo").src = "images/logo/APTA_LOGO.webp";  //changes logo
+    if (document.getElementById("apta")) document.getElementById("apta").src = "images/logo/APTA_LOGO.webp"; //changes logo
+    if (document.getElementById("bglogo")) document.getElementById("bglogo").src = "images/logo/APTA_LOGO.webp";  //changes logo
+    if (document.getElementById("throbber")) document.getElementById("throbber").src = "images/THROBBER.webp";  //changes logo
 }
 
 const disableDarkMode = () => {
     document.body.classList.remove("darkmode")
     localStorage.setItem("darkmode", null)
-    document.getElementById("apta").src = "images/logo/APTA_LOGO_light.webp";  //changes logo
-    document.getElementById("bglogo").src = "images/logo/OCE_LOGO.webp";  //changes logo
+    if (document.getElementById("apta")) document.getElementById("apta").src = "images/logo/APTA_LOGO_light.webp";  //changes logo
+    if (document.getElementById("throbber")) document.getElementById("throbber").src = "images/THROBBER_light.webp";  //changes logo
 }
 
 if (darkmode === "active") enableDarkMode() 
